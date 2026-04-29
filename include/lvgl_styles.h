6eba079b588eadf7f0448cb5ee794b9fca8805a8
Dog-Development <gabrielmascarenas2004@gmail.com>
2026-04-30 00:55:11 +0200

    oopsie poopsies i forgot to add the new files

---
+++ include/lvgl_styles.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <lvgl.h>
+
+// static lv_theme_t default_purple;
+extern lv_style_t style_home;
+extern lv_style_t style_clockContainers;
+extern lv_style_t style_clocks;
+
+void setStyles();
